e9e90d910fc53b0f23d55ea46588e9246c7a74b5
Darin Morrison <freebroccolo@users.noreply.github.com>
2018-04-03 19:02:38 -0600

    Restart merlin on changes to _build

--- src/bin/server/feature/didChangeWatchedFiles.ts
+++ src/bin/server/feature/didChangeWatchedFiles.ts
@@ -1,4 +1,4 @@
-import * as path from "path";
+import * as Path from "path";
 import * as LSP from "vscode-languageserver-protocol";
 import URI from "vscode-uri";
 import * as command from "../command";
@@ -7,17 +7,23 @@ import Session from "../session";
 export default function(session: Session): LSP.NotificationHandler<LSP.DidChangeWatchedFilesParams> {
   return async event => {
     for (const id of event.changes) {
-      const p = path.parse(URI.parse(id.uri).path);
-      if (".ml" === p.ext) {
+      const root = session.environment.workspaceRoot();
+      const rawPath = URI.parse(id.uri).path;
+      const path = Path.parse(root ? Path.relative(root, rawPath) : rawPath);
+      switch (path.dir.split(Path.sep)[0]) {
+        case "_build":
+          return command.restartMerlin(session);
+      }
+      if (".ml" === path.ext) {
         return session.indexer.refreshSymbols(id);
       }
-      if (".re" === p.ext) {
+      if (".re" === path.ext) {
         return session.indexer.refreshSymbols(id);
       }
-      if (".merlin" === p.base) {
+      if (".merlin" === path.base) {
         return command.restartMerlin(session);
       }
-      if ("command-exec" === p.name) {
+      if ("command-exec" === path.name) {
         return command.restartMerlin(session);
       }
     }
